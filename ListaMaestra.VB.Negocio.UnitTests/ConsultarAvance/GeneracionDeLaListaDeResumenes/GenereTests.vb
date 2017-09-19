9ba7bf51f19ddb54ea5c3acd341e9184aedd94e6
Oscar <oscar.centeno@outlook.com>
2017-09-19 11:39:05 -0600

    Los resumenes no deben nunca venir nulos. El contenido es responsabilidad del algoritmo que lo genera.

--- ListaMaestra.VB.Negocio.UnitTests/ConsultarAvance/GeneracionDeLaListaDeResumenes/GenereTests.vb
+++ ListaMaestra.VB.Negocio.UnitTests/ConsultarAvance/GeneracionDeLaListaDeResumenes/GenereTests.vb
@@ -4,7 +4,7 @@ Namespace ConsultarAvance.GeneracionDeResumenDeUnaIteracion_Tests
     <TestClass()> Public Class GenereTests
         Inherits EscenariosDeDatos
 
-        <TestMethod()> Public Sub NoHayIteraciones()
+        <TestMethod()> Public Sub GeneraUnaListaVaciaSiNoHayIteraciones()
             Dim esperado As Integer = 0
 
             Dim historias As IEnumerable(Of Historia) = GenereLaListaDeHistorias()
@@ -15,7 +15,7 @@ Namespace ConsultarAvance.GeneracionDeResumenDeUnaIteracion_Tests
             Assert.AreEqual(esperado, obtenido)
         End Sub
 
-        <TestMethod()> Public Sub HayUnaIteracion()
+        <TestMethod()> Public Sub GeneraUnaListaConUnResumenSiHayUnaIteracion()
             Dim esperado As Integer = 1
 
             Dim historias As IEnumerable(Of Historia) = GenereLaListaDeHistorias()
@@ -26,7 +26,7 @@ Namespace ConsultarAvance.GeneracionDeResumenDeUnaIteracion_Tests
             Assert.AreEqual(esperado, obtenido)
         End Sub
 
-        <TestMethod()> Public Sub HayVariasIteraciones()
+        <TestMethod()> Public Sub GeneraUnaListaConUnResumenPorCadaIteracion()
             Dim esperado As Integer = 4
 
             Dim historias As IEnumerable(Of Historia) = GenereLaListaDeHistorias()
@@ -35,6 +35,14 @@ Namespace ConsultarAvance.GeneracionDeResumenDeUnaIteracion_Tests
             Dim obtenido = lista.Count
 
             Assert.AreEqual(esperado, obtenido)
+        End Sub
+
+        <TestMethod()> Public Sub SiempreGeneraResumenesConContenido()
+            Dim historias As IEnumerable(Of Historia) = GenereLaListaDeHistorias()
+            Dim iteraciones As IEnumerable(Of Integer) = {1, 2, 3, 4}
+            Dim lista = GeneracionDeLaListaDeResumenes.Genere(historias, iteraciones)
+
+            CollectionAssert.AllItemsAreNotNull(lista)
         End Sub
     End Class
 End Namespace
